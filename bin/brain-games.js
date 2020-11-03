7bd72c413d8d7b7ad4b87f5ce395b2b02ac706e1
Vadim Sorokin <k500i90@gmail.com>
2020-11-03 13:02:45 +0300

    Step 5: brain-even

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,4 +2,5 @@
 import cli from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
-cli();
+
+export default cli();
--- src/cli.js
+++ src/cli.js
@@ -3,4 +3,5 @@ import promptly from 'promptly';
 export default async () => {
   const name = await promptly.prompt('May I have your name? ');
   console.log(`Hello, ${name}!`);
+  return name;
 };
